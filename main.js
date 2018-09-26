02951b41f8694bd3a2a5973efc2f73bd7cdc7fec
freetes'Mac <freetes@foxmail.com>
2018-09-26 20:07:11 +0800

    beauty

---
+++ main.js
@@ -0,0 +1,31 @@
+(function getData(){
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', './data.json', true)
+  
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
+      let data = JSON.parse(xhr.responseText)
+      let chs = data.chs
+      const container = document.querySelector('div#container')
+  
+      for(let item of chs){
+        let div = document.createElement('div')
+  
+        div.innerHTML = `
+          <a onclick="goto('/Front-end-diary/${item.path}/')">${item.title}</a>
+          <p>${item.description}</p>
+          <p style="text-align: right">${item.date}</p>
+        `
+        container.appendChild(div)
+      }
+    }
+  }
+
+  xhr.send()
+})()
+
+var iframe = document.querySelector('iframe')
+
+function goto(url){
+  iframe.setAttribute('src', url)
+}
